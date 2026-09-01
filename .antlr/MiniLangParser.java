6d4cc1577ad511fede07aa21ab6e3b48938380fb
JorgeCachyOS <yorkis.gamer32@gmail.com>
2026-08-31 21:05:31 -0300

    adiciona arquivos de gramática e lexer para a linguagem MiniLang

---
+++ .antlr/MiniLangParser.java
@@ -0,0 +1,189 @@
+// Generated from MiniLang.g4 by ANTLR 4.13.2
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue", "this-escape"})
+public class MiniLangParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.13.2", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		PROGRAMA=1, INICIO=2, FIM=3, PV=4, PONTO=5, ID=6, WS=7;
+	public static final int
+		RULE_programa = 0, RULE_bloco = 1;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"programa", "bloco"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'programa'", "'inicio'", "'fim'", "';'", "'.'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, "PROGRAMA", "INICIO", "FIM", "PV", "PONTO", "ID", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "MiniLang.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public MiniLangParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class ProgramaContext extends ParserRuleContext {
+		public TerminalNode PROGRAMA() { return getToken(MiniLangParser.PROGRAMA, 0); }
+		public TerminalNode ID() { return getToken(MiniLangParser.ID, 0); }
+		public TerminalNode PV() { return getToken(MiniLangParser.PV, 0); }
+		public BlocoContext bloco() {
+			return getRuleContext(BlocoContext.class,0);
+		}
+		public TerminalNode PONTO() { return getToken(MiniLangParser.PONTO, 0); }
+		public TerminalNode EOF() { return getToken(MiniLangParser.EOF, 0); }
+		public ProgramaContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_programa; }
+	}
+
+	public final ProgramaContext programa() throws RecognitionException {
+		ProgramaContext _localctx = new ProgramaContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_programa);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(4);
+			match(PROGRAMA);
+			setState(5);
+			match(ID);
+			setState(6);
+			match(PV);
+			setState(7);
+			bloco();
+			setState(8);
+			match(PONTO);
+			setState(9);
+			match(EOF);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class BlocoContext extends ParserRuleContext {
+		public TerminalNode INICIO() { return getToken(MiniLangParser.INICIO, 0); }
+		public TerminalNode FIM() { return getToken(MiniLangParser.FIM, 0); }
+		public BlocoContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_bloco; }
+	}
+
+	public final BlocoContext bloco() throws RecognitionException {
+		BlocoContext _localctx = new BlocoContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_bloco);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(11);
+			match(INICIO);
+			setState(12);
+			match(FIM);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\u0004\u0001\u0007\u000f\u0002\u0000\u0007\u0000\u0002\u0001\u0007\u0001"+
+		"\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0000\u0001\u0000"+
+		"\u0001\u0000\u0001\u0001\u0001\u0001\u0001\u0001\u0001\u0001\u0000\u0000"+
+		"\u0002\u0000\u0002\u0000\u0000\f\u0000\u0004\u0001\u0000\u0000\u0000\u0002"+
+		"\u000b\u0001\u0000\u0000\u0000\u0004\u0005\u0005\u0001\u0000\u0000\u0005"+
+		"\u0006\u0005\u0006\u0000\u0000\u0006\u0007\u0005\u0004\u0000\u0000\u0007"+
+		"\b\u0003\u0002\u0001\u0000\b\t\u0005\u0005\u0000\u0000\t\n\u0005\u0000"+
+		"\u0000\u0001\n\u0001\u0001\u0000\u0000\u0000\u000b\f\u0005\u0002\u0000"+
+		"\u0000\f\r\u0005\u0003\u0000\u0000\r\u0003\u0001\u0000\u0000\u0000\u0000";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
